fe622e5606c82fcb6725da38c31a006de6e48c0e
Michelle <mi.meyer@hotmail.ch>
2017-05-30 09:31:41 +0200

    ISerieRepository added

---
+++ SerienMVC/SerienMVC/Repositories/ISerieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerienMVC.Repositories
+{
+    interface ISerieRepository:IRepository<Serie>
+    {
+        Serie GetSerieWithActor(int id);
+        Serie GetSerieWithGenre(int id);
+    }
+}
