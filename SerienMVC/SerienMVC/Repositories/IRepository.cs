4a068d6674942bb298f53d394aa0512bfda6fe00
Michelle <mi.meyer@hotmail.ch>
2017-05-30 09:24:16 +0200

    first part of repository pattern: IRepository, Repository class

---
+++ SerienMVC/SerienMVC/Repositories/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace SerienMVC.Repositories
+{
+    /// <summary>
+    /// Generic repository
+    /// </summary>
+    /// <typeparam name="TEntity">representing a class</typeparam>
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        //void Update(TEntity entity);
+    }
+}
