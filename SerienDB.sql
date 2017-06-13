b5918cccae1e964f9f97222a860be0c63db28199
Misch <mi.meyer@hotmail.ch>
2017-06-13 21:29:09 +0200

    Genres lists in serie view

--- SerienDB.sql
+++ SerienDB.sql
@@ -6,7 +6,6 @@ CREATE TABLE Genre
 (
 	ID integer IDENTITY (1,1) PRIMARY KEY,
 	Name varchar(50) NOT NULL,
-	ON DELETE CASCADE
 )
 go
 CREATE TABLE Serie
@@ -15,7 +14,6 @@ CREATE TABLE Serie
 	Name varchar(50) NOT NULL,
 	Rating integer NULL,
 	ReleaseDate date NOT NULL
-	ON DELETE CASCADE
 )
 go
 CREATE TABLE Actor
@@ -23,24 +21,26 @@ CREATE TABLE Actor
 	ID integer IDENTITY (1,1) PRIMARY KEY,
 	FirstName varchar(50) NOT NULL,
 	LastName varchar(50) NOT NULL,
-	ON DELETE CASCADE
 )
 go
 CREATE TABLE SerieGenre
 (
-	ID integer IDENTITY (1,1) PRIMARY KEY,
-	fk_Genre integer FOREIGN KEY REFERENCES Genre(ID),
-	fk_Serie integer FOREIGN KEY REFERENCES Serie(ID),
-	CONSTRAINT UniqueForeigKeys UNIQUE (fk_Genre, fk_Serie)
+	fk_Genre integer NOT NULL,
+	fk_Serie integer NOT NULL,
+	FOREIGN KEY (fk_Genre) REFERENCES Genre(ID),
+	FOREIGN KEY (fk_Serie) REFERENCES Serie(ID),
+	PRIMARY KEY (fk_Genre, fk_Serie)
 )
 go
 CREATE TABLE SerieActor
 (
-	ID integer IDENTITY (1,1) PRIMARY KEY,
-	fk_Serie integer FOREIGN KEY REFERENCES Serie(ID),
-	fk_Actor integer FOREIGN KEY REFERENCES Actor(ID),
-	CONSTRAINT UniqueForeigKeys UNIQUE (fk_Actor, fk_Serie)
+	fk_Serie integer NOT NULL,
+	fk_Actor integer NOT NULL,	
+	FOREIGN KEY (fk_Actor) REFERENCES Actor(ID),
+	FOREIGN KEY (fk_Serie) REFERENCES Serie(ID),
+	PRIMARY KEY (fk_Actor, fk_Serie)	
 )
+
 go
 
 INSERT INTO Genre VALUES ('Action')
@@ -53,9 +53,9 @@ INSERT INTO Actor VALUES ('Grant','Gustin')
 INSERT INTO Actor VALUES ('Ellen','Pompeo')
 INSERT INTO Actor VALUES ('Sandra','Oh')
 
-INSERT INTO Serie VALUES ('13 Reasons Why', 9 , convert(date,'31.03.2017',104))
-INSERT INTO Serie VALUES ('The Flash', 8 , convert(date,'07.10.2014',104))
-INSERT INTO Serie VALUES ('Greys Anatomy', 8 ,convert(date,'27.03.2005',104))
+INSERT INTO Serie VALUES ('13 Reasons Why', 9 , '2017-03-31')
+INSERT INTO Serie VALUES ('The Flash', 8 ,'2014-10-07')
+INSERT INTO Serie VALUES ('Greys Anatomy', 8 ,'2005-03-27')
 
 INSERT INTO SerieActor VALUES (1,1)
 INSERT INTO SerieActor VALUES (1,2)
@@ -64,5 +64,6 @@ INSERT INTO SerieActor VALUES (3,4)
 INSERT INTO SerieActor VALUES (3,5)
 
 INSERT INTO SerieGenre VALUES (1,2)
+INSERT INTO SerieGenre VALUES (2,2)
 INSERT INTO SerieGenre VALUES (3,1)
 INSERT INTO SerieGenre VALUES (3,3)
